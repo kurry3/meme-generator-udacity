e12c64d9788900c1db0683ad12a9d511d39b1b65
kurry <leong3@purdue.edu>
2021-06-23 17:25:16 -0500

    Update meme.py

--- meme.py
+++ meme.py
@@ -48,8 +48,11 @@ if __name__ == "__main__":
     # body - quote body to add to the image
     # author - quote author to add to the image
     parser = argparse.ArgumentParser()
-    parser.add_argument("path", type=str, help='path to image file')
-    parser.add_argument("body", type=str, help='quote body to add to the image')
-    parser.add_argument("author", type=str, help='quote author to add to the image')
+    parser.add_argument("path", type=str,
+                        help='path to image file')
+    parser.add_argument("body", type=str,
+                        help='quote body to add to the image')
+    parser.add_argument("author", type=str,
+                        help='quote author to add to the image')
     args = parser.parse_args()
     print(generate_meme(args.path, args.body, args.author))
