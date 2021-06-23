a49b3f2eedb68981bf5117526db05832ba77604c
kurry <leong3@purdue.edu>
2021-06-23 17:21:14 -0500

    Update MemeEngine.py

--- MemeGenerator/MemeEngine.py
+++ MemeGenerator/MemeEngine.py
@@ -17,14 +17,16 @@ class MemeEngine:
     def make_meme(self, img_path, text, author, width=500) -> str:
         try:
             output_path = f"./static/meme_{random.randint(0, 255)}.jpg"
-            save_path = os.path.join(self.output_dir, output_path.replace('./static/', ''))
+            save_path = os.path.join(self.output_dir,
+                                     output_path.replace('./static/', ''))
             img = Image.open(img_path)
             scale = width/img.size[1]
             resized_img = img.resize((width, int(scale*img.size[0])))
             draw = ImageDraw.Draw(resized_img)
             txt = text + "\n-" + author
             font = ImageFont.truetype("arial.ttf", 25)
-            draw.multiline_text((50, 50), txt, fill='white', stroke_fill='black', font=font)
+            draw.multiline_text((50, 50), txt,
+                                fill='white', stroke_fill='black', font=font)
             resized_img.save(save_path)
             return output_path
         except Exception as e:
